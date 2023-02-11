93a5eae812540e4fc8b76338f5e6b37bae5a6d64
Florin <48189025+FlorinInfo@users.noreply.github.com>
2023-02-11 11:11:20 +0200

    Added webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,19 @@ const htmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     mode:"development",
     entry:{
-       main: path.resolve(__dirname, "src/index.js")
+       main: path.resolve(__dirname, "src/index.js"),
     },
     output:{
         path:path.resolve(__dirname, "dist"),
         filename:"[name][contenthash].js",
+    },
+    devServer: {
+        static: {directory:path.resolve(__dirname, "dist")},
+        port:3000,
+        open:true,
+        hot:true,
+        compress:true,
+        historyApiFallback:true
     },
     module:{
         rules:[
